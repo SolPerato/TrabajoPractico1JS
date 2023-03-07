cde3a44e4134d40496cd4eeab7b31a1680a5d737
Sol Perato <solperato27@gmail.com>
2023-03-06 22:42:36 -0300

    Ejercicio 6 - seleccion de numero mayor

---
+++ js/6-ejercicio6.js
@@ -0,0 +1,8 @@
+//6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+var n1 = prompt("Escribe un número");
+var n2 = prompt("Escribe otro número");
+if (n1 > n2) {
+document.write(n1);
+} else {
+document.write(n2);
+}
