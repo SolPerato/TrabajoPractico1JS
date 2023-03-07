297bda80dda36f2f107e128a621c9619fb53e8ec
Sol Perato <solperato27@gmail.com>
2023-03-06 22:52:07 -0300

    Ejercicio 8 - ingreso de numero y respuesta si el mismo es divisible en 2

---
+++ js/8-ejercicio8.js
@@ -0,0 +1,7 @@
+//8.- Escribe un programa que pida un número y diga si es divisible por 2
+var n1 = prompt("Escribe un número");
+if (n1 % 2 === 0) {
+document.write("Es divisible por 2");
+} else {
+document.write("No es divisible por 2");
+}
